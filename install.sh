86b3ae6c1a4ee49b9cba438ac9e76e99aa31ffc9
Michael Morgan <mickmorgan@users.noreply.github.com>
2015-03-17 16:58:50 +0000

    Updated install script.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-# Create symlinks from dotfiles/ to the appropriate config location
+#! /usr/bin/zsh
+
+# Create symlinks from dotfiles repo to the appropriate config location
 
 ln -fs `pwd`/git/.gitconfig ~/.gitconfig
 echo "Git config symlinked to ~/.gitconfig"
@@ -8,3 +10,9 @@ echo "i3 config symlinked to ~/.i3/config"
 
 ln -fs `pwd`/xdefaults/.Xdefaults ~/.Xdefaults
 echo ".Xdefaults symlinked to ~/.Xdefaults"
+
+ln -fs `pwd`/aliases/.aliases ~/.aliases
+echo ".aliases symlinked to ~/.aliases"
+
+ln -fs `pwd`/zsh/.zshrc ~/.zshrc
+echo ".zshrc symlinked to ~/.zshrc"
